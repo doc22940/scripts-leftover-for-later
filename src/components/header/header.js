71234af6a4190bb347827e87c8c0b20869fe3aa6
Daniel Schulz <daniel.schulz@spiegel.de>
2019-11-03 21:01:41 +0100

    add event bus

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,13 @@
+const eventBus = require('../../helpers/eventbus.js');
 module.exports = class Header {
     constructor(el) {
         this.el = el;
-        console.log('header initialized', this.el);
+
+        this.subscription = eventBus.subscribe(
+            "onHeaderReady",
+            message => console.log(`printing:`, message)
+        )
+
+        eventBus.publish("onHeaderReady", this.el);
     }
 }
