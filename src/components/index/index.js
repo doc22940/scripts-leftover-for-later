82bab9b119c82fc08e5eb47249eb7e62aab10357
Daniel Schulz <daniel.schulz@spiegel.de>
2019-11-03 22:35:43 +0100

    add viewport manager

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -1,4 +1,10 @@
 import './index.scss';
 import { Component } from '../../helpers/component.js';
 
+const eventBus = require('../../helpers/eventbus.js');
+eventBus.subscribe(
+    "onViewportChange",
+    message => console.log(`viewport changed:`, message)
+)
+
 new Component();
---
+++ src/components/viewportManager/viewportManager.js
@@ -0,0 +1,40 @@
+const eventBus = require('../../helpers/eventbus.js');
+
+module.exports = class ViewportManager {
+    constructor(el) {
+        this.el = el;
+        this.init();
+    }
+
+    init() {
+        if (!window.getComputedStyle(this.el, ':before').getPropertyValue('content')) {
+            console.error('breakpoint check failed');
+            return;
+        }
+
+        this.breakpoint = this.getBreakpoint();
+        this.oldBreakpoint = this.breakpoint;
+
+        this.publishEvent();
+        window.addEventListener('resize', () => { this.onViewportResize(); }, false);
+    }
+
+    getBreakpoint() {
+        return window.getComputedStyle(this.el, ':before').getPropertyValue('content').split('"').filter(Boolean)
+            .join('"');
+    }
+
+    onViewportResize() {
+        this.oldBreakpoint = this.breakpoint;
+        this.breakpoint = this.getBreakpoint();
+
+        if (this.oldBreakpoint !== this.breakpoint) {
+            this.publishEvent();
+        }
+    }
+
+    publishEvent() {
+        this.el.setAttribute('data-breakpoint', this.breakpoint);
+        eventBus.publish("onViewportChange", this.breakpoint);
+    }
+}
