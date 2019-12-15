83a3655bbbbf371fd634e4199ddee94c1761466c
Daniel Schulz <daniel.schulz@spiegel.de>
2019-12-15 15:11:08 +0100

    fix overlay js

--- src/components/overlay/overlay.js
+++ src/components/overlay/overlay.js
@@ -5,17 +5,13 @@ module.exports = class Overlay {
         this.el = el;
         this.assignedEl = el;
         this.openClass = 'overlay-open';
+        this.state = 'closed';
         this.init();
     }
 
     init() {
-        Global.EventBus.subscribe('onMenuOpen', (menu) => {
-            this.openOverlay(menu);
-        });
-
-        Global.EventBus.subscribe('onMenuClose', (menu) => {
-            this.closeOverlay(menu);
-        });
+        Global.EventBus.subscribe('onMenuOpen', (menu) => { this.openOverlay(menu); });
+        Global.EventBus.subscribe('onMenuClose', (menu) => { this.closeOverlay(menu); });
 
         this.el.addEventListener('click', () => {
             this.closeOverlay(true, false);
@@ -23,17 +19,22 @@ module.exports = class Overlay {
     }
 
     openOverlay(assignedEl) {
+        if (this.state === 'opened') { return; }
+
         this.assignedEl = assignedEl;
         this.el.classList.add(this.openClass);
+        this.state = 'open';
         Global.EventBus.publish('onOverlayOpen', this.el);
     }
 
     closeOverlay(force, assignedEl) {
-        if (!force && this.assignedEl && this.assignedEl !== assignedEl) {
-            return;
-        }
+        if (
+            this.state === 'closed'
+            || (!force && this.assignedEl && this.assignedEl !== assignedEl)
+        ) { return; }
 
         this.el.classList.remove(this.openClass);
+        this.state = 'closed';
         Global.EventBus.publish('onOverlayClose', this.el);
         this.assignedEl = undefined;
     }
