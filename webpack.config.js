980d1f530809da695a17788134f6f382e9978b25
Daniel Schulz <daniel.schulz@spiegel.de>
2019-12-31 22:32:28 +0100

    rename build dir to public

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         index: './src/components/index/index.js',
     },
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js',
     },
     module: {
