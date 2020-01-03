464d2fd03cb52515f8b5ff748cf5585659cc4f9b
Daniel Schulz <daniel.schulz@spiegel.de>
2020-01-03 17:13:44 +0100

    add bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -3,70 +3,80 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
-    entry: {
-        index: './src/entry/script.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['dynamic-import-node-babel-7'],
+module.exports = env => {
+    return {
+        mode: env.mode,
+        entry: {
+            index: './src/entry/script.js',
+    
+        },
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            filename: 'bundle.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: ['dynamic-import-node-babel-7'],
+                        },
                     },
+                }, {
+                    test: /\.worker\.js$/,
+                    use: { loader: 'worker-loader' },
+                }, {
+                    test: /\.(html)$/,
+                    use: {
+                        loader: 'html-loader',
+                        options: {
+                            removeComments: false,
+                            collapseWhitespace: false,
+                            interpolate: true,
+                        },
+                    },
+                }, {
+                    test: /\.(scss|sass|css)$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        { loader: 'css-loader', options: { url: false, sourceMap: true } },
+                        {
+                            loader: 'sass-loader',
+                            options: { sourceMap: true },
+                        },
+                    ],
                 },
-            }, {
-                test: /\.worker\.js$/,
-                use: { loader: 'worker-loader' },
-            }, {
-                test: /\.(html)$/,
-                use: {
-                    loader: 'html-loader',
-                    options: {
-                        removeComments: false,
-                        collapseWhitespace: false,
-                        interpolate: true,
-                    },
-                },
-            }, {
-                test: /\.(scss|sass|css)$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    { loader: 'css-loader', options: { url: false, sourceMap: true } },
-                    {
-                        loader: 'sass-loader',
-                        options: { sourceMap: true },
-                    },
-                ],
-            },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                hash: true,
+                title: 'Some Basic Components',
+                template: './src/entry/document.html',
+                filename: './index.html', // relative to root of the application
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'styles.css',
+                chunkFilename: '[id].css',
+            }),
+            new ScriptExtHtmlWebpackPlugin({
+                preload: /\.js$/,
+                defaultAttribute: 'defer',
+            }),
+            new CopyPlugin([
+                { from: './src/assets/robots.txt', to: '.' },
+            ]),
+            new BundleAnalyzerPlugin({
+                analyzerMode: env.stats ? 'static' : 'disabled',
+                openAnalyzer: true,
+                reportFilename: 'report/index.html',
+            }),
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            hash: true,
-            title: 'Some Basic Components',
-            template: './src/entry/document.html',
-            filename: './index.html', // relative to root of the application
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'styles.css',
-            chunkFilename: '[id].css',
-        }),
-        new ScriptExtHtmlWebpackPlugin({
-            preload: /\.js$/,
-            defaultAttribute: 'defer',
-        }),
-        new CopyPlugin([
-            { from: './src/assets/robots.txt', to: '.' },
-        ]),
-    ],
+    }
 };
